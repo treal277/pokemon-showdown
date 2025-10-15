ba5c3efbcb83c45098fc4d109cba9a1896df88c0
treal277 <totallyrealemail277@gmail.com>
2025-10-15 14:12:44 -0500

    Update formats-data.ts

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -6045,9 +6045,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "Illegal",
 	},
 	slowpose: {
+		isNonstandard: "Custom",
 		tier: "UU",
 	},
 	slowtow: {
+		isNonstandard: "Custom",
 		tier: "UU",
 	},
 };
